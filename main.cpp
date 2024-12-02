d50a2eb107c6aa0c7e8f5b3f80b7d16a1b697d44
Wappyplay <wappyplay2007@gmail.com>
2024-12-03 00:26:48 +0200

    new code

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 #define m 5
 int main()
 {
-   const int n = 6;
-   int a[n] = {84, 38, 12};
+   const int n = 666;
+   int a[n] = {84, 38333, 12};
    srand(time(NULL));
    for(int i=0; i<n; i++)
    //cin >> a[i];
